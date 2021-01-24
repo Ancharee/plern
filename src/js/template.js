4420559d0887595dbda802325ca51f748c377bf2
Min <miiny27@gmail.com>
2021-01-24 16:50:17 +0700

    install gsap

--- src/js/template.js
+++ src/js/template.js
@@ -1,7 +1,40 @@
 import fullpage from 'fullpage.js';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 export class Template {
   init() {
-    new fullpage('#fullpage');
+    this.initFullPage();
+    this.startAnimation();
+  }
+
+  initFullPage() {
+    const section = document.getElementById('fullpage');
+    if (section) {
+      new fullpage('#fullpage');
+    }
+  }
+
+  startAnimation() {
+    const tl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
+    tl.to('.js-header', {
+      ease: 'easeOut',
+      y: 0,
+      duration: 1,
+    });
+    tl.to('.js-download-bar', {
+      ease: 'easeOut',
+      y: 0,
+      duration: 1,
+    }, '-=1');
+    const image1 = document.getElementsByName('js-section-image-1');
+    if (image1) {
+      tl.to('.js-section-image-1', {
+        ease: 'easeOut',
+        y: 0,
+        duration: 3,
+      });
+    }
   }
 }

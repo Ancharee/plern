7870fdb61eb909e08f60822ff5d6403f3c67e417
Min <miiny27@gmail.com>
2021-01-25 20:55:57 +0700

    adjust sections

--- src/js/template.js
+++ src/js/template.js
@@ -1,40 +1,57 @@
-import fullpage from 'fullpage.js';
+// import fullpage from 'fullpage.js';
+import fullpage from 'fullpage.js/dist/fullpage.extensions.min';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export class Template {
+  constructor() {
+    this.container = document.getElementById('fullpage');
+    this.header = document.getElementsByClassName('js-header');
+    this.image = document.getElementsByClassName('js-section-image-1');
+    this.downloadBar = document.getElementsByClassName('js-download-bar');
+  }
+
   init() {
     this.initFullPage();
-    this.startAnimation();
   }
 
   initFullPage() {
-    const section = document.getElementById('fullpage');
-    if (section) {
-      new fullpage('#fullpage');
+    if (this.container) {
+      new fullpage('#fullpage', {
+        parallax: true,
+        parallaxOptions: {
+          type: 'reveal',
+          percentage: 62,
+          property: 'translate'
+        },
+        scrollingSpeed: 1000,
+        autoScrolling: true,
+        fitToSection: false,
+        // scrollBar: true,
+        afterRender: () => {
+          this.animationToFirstSection();
+        },
+      });
     }
   }
 
-  startAnimation() {
-    const tl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
-    tl.to('.js-header', {
-      ease: 'easeOut',
-      y: 0,
-      duration: 1,
-    });
-    tl.to('.js-download-bar', {
-      ease: 'easeOut',
-      y: 0,
-      duration: 1,
-    }, '-=1');
-    const image1 = document.getElementsByName('js-section-image-1');
-    if (image1) {
-      tl.to('.js-section-image-1', {
-        ease: 'easeOut',
-        y: 0,
-        duration: 3,
-      });
+  animationToFirstSection() {
+    const tl = gsap.timeline({ repeat: 0, repeatDelay: 0, defaults: { ease: 'easeOut', y: 0 } });
+    tl.to(this.header, { duration: 1, delay: 2 });
+    if (this.hasDownloadBarVisible) {
+      tl.to(this.downloadBar, { duration: 1 }, '-=1');
     }
+    if (this.hasImageVisible) {
+      tl.to(this.image, { duration: 3 });
+    }
+  }
+
+  hasDownloadBarVisible() {
+    return this.downloadBar.offsetHeight !== 0 && this.downloadBar.offsetWidth !== 0;
+  }
+
+  hasImageVisible() {
+    return this.image;
   }
 }

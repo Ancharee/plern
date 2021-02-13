22fd6c399c32899255844f3fe2aef0a0a384622b
Min <miiny27@gmail.com>
2021-02-13 15:02:16 +0700

    add contact page

--- src/js/components/form.js
+++ src/js/components/form.js
@@ -1,24 +1,28 @@
 import MicroModal from 'micromodal';
 
 export class Form {
+  constructor() {
+    this.form = document.getElementsByClassName('js-submit');
+    this.modal = document.getElementsByClassName('js-modal-confirm');
+  }
+
   init() {
-    MicroModal.init();
-    const form = document.getElementsByClassName('js-submit');
-    if (form.length) {
-      form[0].addEventListener('click', () => {
+    if (this.form.length) {
+      this.form[0].addEventListener('click', () => {
         this.submit();
       });
-      document
-        .getElementsByClassName('js-modal-confirm')[0]
-        .addEventListener('click', () => {
-          this.openModalDone();
-        });
+    }
+    if (this.modal.length) {
+      MicroModal.init();
+      this.modal[0].addEventListener('click', () => {
+        this.openModalDone();
+      });
     }
   }
 
   submit() {
     const form = document.getElementsByClassName('js-form')[0];
-    const controls = form.getElementsByTagName('input');
+    const controls = form.getElementsByClassName('form-control');
     let errors = 0;
     for (let index = 0; index < controls.length; index++) {
       const control = controls[index];
@@ -31,7 +35,7 @@ export class Form {
       }
     }
 
-    if (errors <= 0) {
+    if (errors <= 0 && this.modal.length) {
       this.openModalOTP();
     }
   }
